0cf9224a4ea8bce910eaf95db79784e895fb3c8f
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2024-11-07 12:54:04 +0100

    feat(spawner): Define a more complexe spawer for the game to manage the mole spawn and rate

--- Assets/Scripts/Managers/SpawnerManager.cs
+++ Assets/Scripts/Managers/SpawnerManager.cs
@@ -4,34 +4,90 @@ using System.Collections;
 
 public class SpawnerManager : MonoBehaviour
 {
-	public GameObject enemyPrefab;
-	public int numEnemies = 1;
-	public float spawnInterval = 10f;
-	private List<GameObject> enemies = new List<GameObject>();
+	[Header("General Settings")]
+	[SerializeField] private List<MoleHole> _molesHoles = new();
+	[SerializeField] private float _maxSpawnInterval = 2f;
+	[SerializeField] private float _minSpawnInterval = 0.5f;
+	[SerializeField] private int _maxSpawnEnemies = 5;
+	[SerializeField] private float _safeMoleSpawnRate = 0.2f;
 
-	void Start()
+	[Header("Enemies Prefab")]
+	[SerializeField] private GameObject _molePrefab;
+	[SerializeField] private GameObject _safeMolePrefab;
+
+	[Header("SO Float References")]
+	[SerializeField] private FloatVariable _gameHP;
+	[SerializeField] private FloatVariable _rate;
+
+	private float _currentSpawnInterval;
+	private readonly Dictionary<int, GameObject> _enemies = new();
+
+	public class MoleHole
 	{
+		public int index;
+		public Transform transform;
+	}
+
+	private void Start()
+	{
+		_currentSpawnInterval = _maxSpawnInterval;
 		StartCoroutine(SpawnEnemiesAtInterval());
+	}
+
+	private void Update()
+	{
+		if (_gameHP.value <= 0)
+		{
+			StopCoroutine(SpawnEnemiesAtInterval());
+			KillAllEnemies();
+			gameObject.SetActive(false);
+		}
+		else
+		{
+			_currentSpawnInterval = Mathf.Max(_minSpawnInterval, _currentSpawnInterval - (_rate.value * Time.deltaTime));
+		}
 	}
 
 	IEnumerator SpawnEnemiesAtInterval()
 	{
 		while (true)
 		{
-			SpawnEnemies();
-			spawnInterval = Mathf.Max(2f, spawnInterval * 0.95f);
-			yield return new WaitForSeconds(spawnInterval);
+			if (_enemies.Count < _maxSpawnEnemies) SpawnEnemy();
+			yield return new WaitForSeconds(_currentSpawnInterval);
 		}
 	}
 
-	void SpawnEnemies()
+	private void SpawnEnemy()
 	{
-		for (int i = 0; i < numEnemies; i++)
+		MoleHole moleHole = GetRandomFreeMoleHole();
+		GameObject enemy;
+		if (Random.value < _safeMoleSpawnRate)
 		{
-			// TODO
-			Vector3 spawnPosition = Vector3.zero;
-			GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
-			enemies.Add(enemy);
+			enemy = Instantiate(_safeMolePrefab, moleHole.transform.position, Quaternion.identity);
 		}
+		else
+		{
+			enemy = Instantiate(_molePrefab, moleHole.transform.position, Quaternion.identity);
+		}
+		_enemies[moleHole.index] = enemy;
+	}
+
+	private MoleHole GetRandomFreeMoleHole()
+	{
+		int randomIndex;
+		do
+		{
+			randomIndex = Random.Range(0, _molesHoles.Count);
+		} while (_enemies.ContainsKey(_molesHoles[randomIndex].index));
+		return _molesHoles[randomIndex];
+	}
+
+	private void KillAllEnemies()
+	{
+		foreach (var enemy in _enemies.Values)
+		{
+			enemy.GetComponent<Enemy>().ForceKill();
+		}
+		_enemies.Clear();
 	}
 }
